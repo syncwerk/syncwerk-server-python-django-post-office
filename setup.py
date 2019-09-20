ee4d4019516b66d362beddecf21fded5f9c44729
Alexander Jackson <alexander.jackson@syncwerk.com>
2019-09-20 14:12:57 +0200

    first commit

---
+++ setup.py
@@ -0,0 +1,69 @@
+# -*- coding: utf-8 -*-
+import sys
+from setuptools import setup
+from setuptools.command.test import test as TestCommand
+
+
+class Tox(TestCommand):
+
+    def initialize_options(self):
+        TestCommand.initialize_options(self)
+        self.tox_args = None
+
+    def finalize_options(self):
+        TestCommand.finalize_options(self)
+        self.test_args = []
+        self.test_suite = True
+
+    def run_tests(self):
+        # import here, cause outside the eggs aren't loaded
+        import tox
+        import shlex
+        args = self.tox_args
+        if args:
+            args = shlex.split(self.tox_args)
+        errno = tox.cmdline(args=args)
+        sys.exit(errno)
+
+
+TESTS_REQUIRE = ['tox >= 2.3']
+
+
+setup(
+    name='django-post_office',
+    version='3.0.4',
+    author='Selwin Ong',
+    author_email='selwin.ong@gmail.com',
+    packages=['post_office'],
+    url='https://github.com/ui/django-post_office',
+    license='MIT',
+    description='A Django app to monitor and send mail asynchronously, complete with template support.',
+    long_description=open('README.rst').read(),
+    zip_safe=False,
+    include_package_data=True,
+    package_data={'': ['README.rst']},
+    install_requires=['django>=1.8', 'jsonfield'],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+    tests_require=TESTS_REQUIRE,
+    extras_require={
+        'test': TESTS_REQUIRE,
+    },
+    cmdclass={'test': Tox}
+)
